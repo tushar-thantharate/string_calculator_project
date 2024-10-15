aa25b04145572a1251127f3423e14ff8d121da83
Tushar <tushar>
2024-10-15 20:28:03 +0530

    Added logic to handle newline between numbers

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -6,12 +6,16 @@ class StringCalculator:
         if not numbers:
             return 0
 
+        # Replace newline from the number string with comma.
+        numbers = numbers.replace('\n', ',')
+
         if len(str(numbers)) == 1:
              return int(numbers)
 
         return sum(int(num) for num in numbers.split(',') if num.strip())
 
-# calc = StringCalculator()
+
+calc = StringCalculator()
 # print (calc.add(""))
 # print (calc.add(None))
 
@@ -20,3 +24,7 @@ class StringCalculator:
 # print (calc.add('1,'))
 # print (calc.add('1,2'))
 # print (calc.add('1,2,3,4,5'))
+
+# print (calc.add('1\n'))
+# print (calc.add('1\n2,3'))
+# print (calc.add('1\n2,3\n4,5\n'))
