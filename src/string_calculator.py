4055a8c88656ddcbb92daf1c015fbcf9d7d3f744
Tushar <tushar>
2024-10-15 19:43:00 +0530

    Add logic to handle empty string/Input: “”, Output: 0

---
+++ src/string_calculator.py
@@ -0,0 +1,12 @@
+class StringCalculator:
+    def __init__(self):
+        pass
+
+    def add(self, numbers):
+        if not numbers:
+            return 0
+
+
+# calc = StringCalculator()
+# print (calc.add(""))
+# print (calc.add(None))
