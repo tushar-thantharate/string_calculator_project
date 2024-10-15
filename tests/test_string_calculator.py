46510ee7e73b1b048a6f4c2582cf53cdc338c007
Tushar <tushar>
2024-10-15 19:48:34 +0530

    Added test for Input: “1”, Output: 1/single number

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -2,11 +2,19 @@ import unittest
 from src.string_calculator import StringCalculator
 
 class TestStringCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = StringCalculator()
+
     def test_empty_string(self):
         # Input: “”, Output: 0
-        calc = StringCalculator()
-        result = calc.add("")
+        result = self.calc.add("")
         self.assertEqual(result, 0)
+
+    def test_single_number(self):
+        # Input: “1”, Output: 1
+        result = self.calc.add("1")
+        self.assertEqual(result, 1)
 
 
 if __name__ == '__main__':
