58c32a7fbadbdb3f1017715ace6c212126156685
Tushar <tushar>
2024-10-15 22:11:18 +0530

    Add docstrings and comments for clarity and documentation.

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -3,28 +3,40 @@ class StringCalculator:
         pass
 
     def add(self, number_string):
+        """
+            Takes a string of numbers, which can include custom delimiters and 
+            newline characters, and returns their sum.
+        """
+        # If the input is empty, return 0
         if not number_string:
             return 0
 
-        # Replace newline from the number string with comma.
-        # numbers = numbers.replace('\n', ',')
-
+        # If there is only one character, return same number
         if len(str(number_string)) == 1:
              return int(number_string)
 
         delimiter = ','
+        # Check if the number string starts with a custom delimiter pattern
         if number_string.startswith("//"):
             status, number_string, delimiter = self.get_custom_delimiter(number_string)
+            # exit if no newline found.
             if not status:
                 return 0
 
+        # Split the numbers using the idenfied delimiter
         numbers = self.split_by_delimiter(number_string, delimiter)
 
+        # Check if any negative numbers present and raise an error if found
         self.check_for_negative_numbers(numbers)
 
+        # Compute and return the sum of the numbers
         return self.get_number_sum(numbers)
         
     def get_custom_delimiter(self, number_string):
+        """
+            Extracts a custom delimiter from the input string based on the pattern
+             Pattern: "//[delimiter]\n[numbers…]"
+        """
         newline_exists = number_string.find('\n')
         if newline_exists == -1:
             return False, "Number string must contain a newline for custom delimiter.", ''
@@ -35,20 +47,29 @@ class StringCalculator:
         return True, number_string, delimiter
 
     def split_by_delimiter(self, number_string, delimiter):
+        """
+            Splits the number string using the provided delimiter.
+        """
         return number_string.replace('\n', delimiter).split(delimiter)
 
     def get_number_sum(self, numbers):
-        # skip numbers greater than 1000 in calcualtion
+        """
+            Compute the sum of the provided number strings, skip any 
+            numbers greater than 1000.
+        """
         return sum(int(num) for num in numbers if num.strip() and int(num) <= 1000 if num.strip().isdigit())
 
     def check_for_negative_numbers(self, numbers):
+        """
+            Checks if any number is negative and raise ValueError.
+        """
         negative_numbers = [num for num in numbers if num and int(num) < 0]
         if negative_numbers:
             raise ValueError("Negative numbers not allowed: {}".format(','.join([str(num) for num in negative_numbers])))
 
     
 
-calc = StringCalculator()
+# calc = StringCalculator()
 # print (calc.add(""))
 # print (calc.add(None))
 
@@ -70,5 +91,5 @@ calc = StringCalculator()
 # print (calc.add("1,-2,3"))
 # print (calc.add("1,-2,-3,4"))
 
-print (calc.add("1001,2"))
-print (calc.add("1,1001,2,3,4,1002,5"))
+# print (calc.add("1001,2"))
+# print (calc.add("1,1001,2,3,4,1002,5"))
--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -4,20 +4,34 @@ from src.string_calculator import StringCalculator
 class TestStringCalculator(unittest.TestCase):
 
     def setUp(self):
+        """
+            Sets up an instance of StringCalculator.
+        """
         self.calc = StringCalculator()
 
     def test_empty_string(self):
-        # Input: “”, Output: 0
+        """
+            Test with an empty string. It should return 0
+            # Input: “”, Output: 0
+        """
         result = self.calc.add("")
         self.assertEqual(result, 0)
 
     def test_single_number(self):
-        # Input: “1”, Output: 1
+        """
+            Test with a single number as input. It should return same number.
+            # Input: “1”, Output: 1
+        """
+        
         result = self.calc.add("1")
         self.assertEqual(result, 1)
 
     def test_multiple_numbers(self):
-        # Input: “1,2,3,4,5”, Output: 15
+        """
+            Test with two or more comma separated numbers.
+            # Input: “1,2,3,4,5”, Output: 15
+        """
+        
         result = self.calc.add("1,")
         self.assertEqual(result, 1)
 
@@ -28,16 +42,21 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(result, 15)
 
     def test_newline_delimiter(self):
-        # Input: “1\n2,3,4,5”, Output: 15
+        """
+            Test with newline as delimiters.
+            # Input: “1\n2,3,4,5”, Output: 15
+        """
         result = self.calc.add("1\n2,3")
         self.assertEqual(result, 6)
 
         result = self.calc.add("1\n2,3\n4,5\n")
         self.assertEqual(result, 15)
 
-
     def test_custom_delimiter(self):
-        # Input: “//*\n1*2*3*4*5”, Output: 15
+        """
+            Test with custom delimiters defined by the pattern: //[delimiter]\n[numbers].
+            # Input: “//*\n1*2*3*4*5”, Output: 15
+        """
         result = self.calc.add("//;\n1;2")
         self.assertEqual(result, 3)
 
@@ -52,6 +71,9 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(result, 0)
 
     def test_negative_numbers(self):
+        """
+            Test with a negative number. It should raises a ValueError with an expected message.
+        """
         try:
             self.calc.add("1,-2,3")
         except ValueError as e:
@@ -65,7 +87,9 @@ class TestStringCalculator(unittest.TestCase):
  
 
     def test_skip_large_numbers(self):
-        # skip numbers greater than 1000 in calcualtion
+        """
+            Test with larger than 1000 number. It should be skipped during cumpuation.
+        """
         result = self.calc.add("1001,2")
         self.assertEqual(result, 2)
 
@@ -73,6 +97,9 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(result, 15)
 
     def test_mix_cases(self):
+        """
+            Test with multiple scenarios.
+        """
         result = self.calc.add("1\n2,1001,3,4,5")
         self.assertEqual(result, 15)
 
